6161b0a6bee01c7f4d1460e059a65ac608e76c1b
unknown <langelcubas92@gmail.com>
2024-12-17 13:40:29 -0500

    Added user feature

--- src/features/user/handler.ts
+++ src/features/user/handler.ts
@@ -1,30 +1,45 @@
 import { APIGatewayProxyEvent } from 'aws-lambda';
 import { UserService } from './services/UserService';
 
-export const createUser = async (event: APIGatewayProxyEvent) => {
-  const { body } = event;
-  const user = JSON.parse(body || '');
+export const getUsers = async () => {
+  const users = await UserService.getUsers();
 
-  if (!user) {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ data: users }),
+  };
+}
+
+export const getUserById = async (event: APIGatewayProxyEvent) => {
+  const { userId } = event.pathParameters || {};
+
+  if (!userId) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: 'userId is required' }),
+    };
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ data: null }),
+  };
+}
+
+export const createUser = async (event: APIGatewayProxyEvent) => {
+  const userData = JSON.parse(event.body || '');
+
+  if (!userData) {
     return {
       statusCode: 400,
       body: JSON.stringify({ error: 'User is required' }),
     };
   }
 
-  const createdUser = await UserService.createUser(user);
+  const createdUser = await UserService.createUser(userData);
 
   return {
     statusCode: 200,
     body: JSON.stringify({ data: createdUser }),
-  };
-}
-
-export const getAllUsers = async (event: APIGatewayProxyEvent) => {
-  const users = await UserService.getAllUsers();
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ data: users }),
   };
 }
--- src/features/user/services/UserService.ts
+++ src/features/user/services/UserService.ts
@@ -1,25 +1,16 @@
 import DynamoDb from '../../../services/dynamo-db/DynamoDb';
-import { USERS_TABLE } from '../constants';
 import { User } from '../entities/User';
+import { USERS_TABLE } from '../constants';
 
 export class UserService {
   static tableName = USERS_TABLE;
 
-  static async getAllUsers(): Promise<User[]> {
+  static async getUsers(): Promise<User[]> {
     const params = {
       TableName: UserService.tableName,
     };
 
     return await DynamoDb.scan<User>(params) as User[];
-  }
-
-  static async createUser(user: any): Promise<void> {
-    const params = {
-      TableName: UserService.tableName,
-      Item: user,
-    };
-
-    await DynamoDb.put(params);
   }
 
   static async getUserById(userId: string): Promise<User> {
@@ -29,5 +20,14 @@ export class UserService {
     };
 
     return await DynamoDb.get(params) as User;
+  }
+
+  static async createUser(user: any): Promise<void> {
+    const params = {
+      TableName: UserService.tableName,
+      Item: user,
+    };
+
+    await DynamoDb.put(params);
   }
 }
