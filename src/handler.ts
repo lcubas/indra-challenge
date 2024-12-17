071c15f0d54b0697145aa4cb51aa6010f152179b
lcubas <langelcubas9@gmail.com>
2024-12-16 21:33:16 -0500

    Initial config

---
+++ src/handler.ts
@@ -0,0 +1,12 @@
+import { APIGatewayEvent, Context } from "aws-lambda";
+
+export const hello = async (event: APIGatewayEvent, context: Context) => {
+  console.log(event, context);
+  
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Hello world!",
+    }),
+  };
+};
